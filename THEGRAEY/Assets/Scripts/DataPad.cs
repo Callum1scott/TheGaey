23ee440138ad74c3364f540aa41bf88e45b710e2
Callum1scott <61849378+Callum1scott@users.noreply.github.com>
2022-05-08 17:33:25 -0400

    finish

--- THEGRAEY/Assets/Scripts/DataPad.cs
+++ THEGRAEY/Assets/Scripts/DataPad.cs
@@ -7,17 +7,42 @@ public class DataPad : MonoBehaviour
     private AudioSource audioSource;
     public AudioClip logAudioClip;
 
+    private bool isPlay;
+
     public void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = logAudioClip;
+
+        isPlay = false;
+    }
+
+    private void Update()
+    {
+        if (audioSource.isPlaying == false)
+        {
+            isPlay = false;
+        }
+        else
+        {
+            isPlay = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.gameObject.CompareTag("Player"))
         {
-            audioSource.Play();
+            if(isPlay == false)
+            {
+                audioSource.Play();
+                
+            }
+            else
+            {
+                Debug.Log("playing dumbass");
+            }
         }
     }
 }
